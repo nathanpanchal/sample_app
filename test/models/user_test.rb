21ff3140a287e5e0f0c7c7a710fc993f57cd8f3d
Nathan Panchal <nathanpanchal@gmail.com>
2015-07-11 11:03:52 -0700

    add test for unique email addresses

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,4 +51,10 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @test_user.dup
+    @test_user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
