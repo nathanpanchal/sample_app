85d43ee9ddf19fdc4dc55f9e34c4b0a6d1876309
Nathan Panchal <nathanpanchal@gmail.com>
2015-07-11 10:37:24 -0700

    install minitest reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
