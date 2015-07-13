278805cfecfeecc6c12da4007e5110114cef22a7
Nathan Panchal <nathanpanchal@gmail.com>
2015-07-12 18:40:43 -0700

    normalise emails before saving to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # normalizes the emails saved to the database
+  before_save {self.email = self.email.downcase}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
