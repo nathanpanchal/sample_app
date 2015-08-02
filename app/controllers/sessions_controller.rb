da8484a6e9d40d7a0d80e7c4e09119881b5f2703
Nathan Panchal <nathanpanchal@gmail.com>
2015-08-01 22:49:03 -0700

    refactor sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # returns false if the user with that email is not found in the database
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user 
+      log_in user
+      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
