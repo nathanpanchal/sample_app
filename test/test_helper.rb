a0fc8abe651391427fac96962a174e05f86f908f
Nathan Panchal <nathanpanchal@gmail.com>
2015-08-12 08:30:12 -0700

    add comments to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,11 +17,14 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  # Creates a sessions hash depending on the type of test the method is called on.
   def log_in_as(user, options = {})
     password = options[:password] || 'password'
     remember_me = options[:remember_me] || '1'
     if integration_test?
       post login_path, session: {email: user.email, password: password, remember_me: remember_me}
+    # Used for model and controller tests where we cannot post to the sessions path. Insead we must
+    # modify the session hash directly.
     else
       session[:user_id] = user_id
     end
@@ -31,6 +34,7 @@ class ActiveSupport::TestCase
 
     # Returns true inside an integration test.
     def integration_test?
+      # Post via redirect is only defined in integration tests via inheritance.
       defined?(post_via_redirect)
     end
 
