bdef2a6dd901f7a744d70e2bac83c009ca3ebfb7
Nathan Panchal <nathanpanchal@gmail.com>
2015-08-16 19:54:05 -0700

    fix spelling error in integration test and allow nil passwords during edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -26,7 +26,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                    password: '',
                                    password_confirmation: ''}
     assert_not flash.empty?
-    asser_redirected_to @user
+    assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
