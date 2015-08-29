ff8209da1325c4bf4af7de59344ddcb5caeeed9c
Nathan Panchal <nathanpanchal@gmail.com>
2015-08-28 23:26:08 -0700

    finish integration test for users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,6 +11,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select div.pagination
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+    end
   end
 
 end
