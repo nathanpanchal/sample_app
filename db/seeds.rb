6abb8a50e9e0e5597cf4a5fa0c5f868671af2168
Nathan Panchal <nathanpanchal@gmail.com>
2015-08-30 21:09:25 -0700

    fix bug in seed document

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 User.create!(name: "Example User", 
              email: "example@railstutorial.org", 
              password: "12345678", 
-             password_confirmation: "12345678"
+             password_confirmation: "12345678",
              admin: true)
 
 99.times do |n|
