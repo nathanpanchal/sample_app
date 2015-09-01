b5a89cdbc5929b4f5b6808edc17b6cb82e9f41cb
Nathan Panchal <nathanpanchal@gmail.com>
2015-08-31 21:47:52 -0700

    add admin authentication to destry action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def new
     @user = User.new
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'User Deleted'
+    redirect_to users_url
   end
 
   def index
@@ -60,5 +67,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
+    end
+
+    # Confirms an admin user.
+    def admin_user
+      # Employs the boolean nature of the admin attribute on the users table. If the current user is not an admin.
+      redirect_to(root_url) unless current_user.admin?
     end
 end
