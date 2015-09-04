42daad22478ec54d22f29d1194fa194fcc3eb05c
Nathan Panchal <nathanpanchal@gmail.com>
2015-09-04 16:47:46 -0700

    add test for non admin user index

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,16 +7,6 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     @non_admin = users(:archer)
   end
 
-  test "index include pagination" do
-    log_in_as(@user)
-    get users_path
-    assert_template 'users/index'
-    assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-    end
-  end
-
   test 'index as admin including pagination and delete links' do
     log_in_as(@admin)
     get users_path
@@ -24,7 +14,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select 'div.pagination'
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[hfref=?]', user_path(user), text: user.name
+      assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
       end
@@ -32,6 +22,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
+  end
+
+  test 'index as a non-admin' do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
   end
 
 end
