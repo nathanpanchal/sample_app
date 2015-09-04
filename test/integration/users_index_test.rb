8122e45fb77085e7d11171a82914f291c1137400
Nathan Panchal <nathanpanchal@gmail.com>
2015-09-04 15:41:20 -0700

    add integration test for testing non admin delete requests

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -17,10 +17,21 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test 'index as admin including pagination and delete link' do
+  test 'index as admin including pagination and delete links' do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[hfref=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
   end
 
 end
