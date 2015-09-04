1a7703f7be1424a2a5471d3b52bc9ae4b503f99e
Nathan Panchal <nathanpanchal@gmail.com>
2015-09-03 21:41:51 -0700

    start adding text for admin user logging in with pagination

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @admin = users(:michael)
+    @non_admin = users(:archer)
   end
 
   test "index include pagination" do
@@ -14,6 +15,12 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test 'index as admin including pagination and delete link' do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
   end
 
 end
