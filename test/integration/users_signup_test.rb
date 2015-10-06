f22b7831213e4ad3070df9f4eb87b3bec1c929a6
Nathan Panchal <nathanpanchal@gmail.com>
2015-10-05 19:36:08 -0700

    finish user signup integration test. Now includes tests for user activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,21 +21,28 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, user: { name: "Example User",
-                                           email: "user@valid.com",
-                                           password: "password",
-                                           password_confirmation: "password" }
+                               email: "user@valid.com",
+                               password: "password",
+                               password_confirmation: "password" }
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
     assert_not user.activated?
-    # Attempt to log in before activation.
+    # Try to log in before activation.
     log_in_as(user)
     assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path('invalid token')
+    assert_not is_logged_in?
     # Valid token, wrong email
-    get edit_account_activatin_path('invalid token')
-    # assert_template 'users/show'
-    # assert_not flash.empty?
-    # assert is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 
 end
