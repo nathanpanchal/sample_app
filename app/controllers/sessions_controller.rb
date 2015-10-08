df58f526288f5a5d1a9b1447bd074fb3c7474311
Nathan Panchal <nathanpanchal@gmail.com>
2015-10-07 20:34:24 -0700

    fixed bug in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     # If the user exists (not nil per line 6) and the password is authenticated log them in.
     if @user && @user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
+      if @user.activated?
+        log_in @user
         # An alternate if/then/else statement. If the remember me box is checked (== '1')
         # then remember the user else forget the user.
         params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-        redirect_back_or user
+        redirect_back_or @user
       else
         message = 'Account not activated. '
         message += 'Check your email for the activation link.'
