6c7a9835b9e002770f4ba102d74f05836cfbd089
Nathan Panchal <nathanpanchal@gmail.com>
2015-11-08 00:41:03 -0800

    add create method to password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,10 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+  end
+
   def edit
   end
 end
