7019b40935dadde9a0292403710874e05866615f
Nathan Panchal <nathanpanchal@gmail.com>
2015-12-05 23:27:54 -0800

    user model fix indenting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   
   # validates the presence and length of the name string
