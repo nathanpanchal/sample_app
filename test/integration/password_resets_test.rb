c62c73b4f0cc174fe426c71cd2d44603443b5bca
Nathan Panchal <nathanpanchal@gmail.com>
2015-12-10 21:45:28 -0800

    test invalid email submission

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -3,9 +3,19 @@ require 'test_helper'
 class PasswordResetsTest < ActionDispatch::IntegrationTest
 
   def setup
-    # clears all pending test deliveries
+    # Clears all pending test deliveries
     ActionMailer::Base.deliveries.clear
     @user = user(:michael)
+  end
+
+  test 'password resets' do
+    get new_password_rests_path
+    assert_template 'password_rest/new'
+    # Post invalid email
+    post password_resets_path, password_reset: {email: ''}
+    assert_not flash.empty?
+    # Test that the user is redirected to the new password reset form
+    assert_template 'password_resets/new'
   end
 
 end
