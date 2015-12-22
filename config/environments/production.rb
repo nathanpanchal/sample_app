e400b5eb3e682727e8ab0df6a6274dd1a7ecef37
Nathan Panchal <nathanpanchal@gmail.com>
2015-12-21 22:08:24 -0800

    finish smtp settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'royal-marks-4929.herokuapp.com'
+  config.action_mailer.default_url_options = {host: host}
+  ActionMailer::Base.stmp_settings = {
+    :address        => 'smtp.sengrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
