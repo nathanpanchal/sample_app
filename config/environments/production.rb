2e6855e53e5861b19cd0bbd9a79ea06dd3b22370
Nathan Panchal <nathanpanchal@gmail.com>
2015-12-22 20:41:46 -0800

    fixed typo in production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'royal-marks-4929.herokuapp.com'
   config.action_mailer.default_url_options = {host: host}
-  ActionMailer::Base.stmp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sengrid.net',
     :port           => '587',
     :authentication => :plain,
