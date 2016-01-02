20d1b1916e47f838454be9e6d6d15b9bb4ac7e40
Nathan Panchal <nathanpanchal@gmail.com>
2016-01-01 17:28:03 -0800

    add test for checking if a micropost is valid

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:michael)
+    @micropost = Micropost.new(content: "Some acceptable content", user_id: @user.id)
+  end
+
+  # tests if a micropost is valid from a database submission standpoint. valid? is an active record
+  # function.
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
 end
