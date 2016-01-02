eb7d73ff5fe427222c55729747c900360cd11f9c
Nathan Panchal <nathanpanchal@gmail.com>
2016-01-02 09:29:32 -0800

    test change to make sure git works

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -7,8 +7,7 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost = Micropost.new(content: "Some acceptable content", user_id: @user.id)
   end
 
-  # tests if a micropost is valid from a database submission standpoint. valid? is an active record
-  # function.
+  # tests if a micropost is valid from a database submission standpoint. valid? is an active record function.
   test 'should be valid' do
     assert @micropost.valid?
   end
