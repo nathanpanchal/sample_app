96ff4053789bd63d4e833a0416b3bc15e87dc9de
Nathan Panchal <nathanpanchal@gmail.com>
2016-01-03 21:18:16 -0800

    add micropost assciation on user controller

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,10 +4,9 @@ class MicropostTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: "Some acceptable content", user_id: @user.id)
+    @micropost = @user.microposts.build(content: 'some valid content')
   end
 
-  # tests if a micropost is valid from a database submission standpoint. valid? is an active record function.
   test 'should be valid' do
     assert @micropost.valid?
   end
