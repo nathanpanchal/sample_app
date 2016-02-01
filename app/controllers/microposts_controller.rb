f58a93c6faeba5c4f32373815c412e9d1aa92192
Nathan Panchal <nathanpanchal@gmail.com>
2016-01-31 23:00:38 -0800

    add create method form micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,14 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
 
   def create
-
+    # current_user defined in session helper inherited from Application controller
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
