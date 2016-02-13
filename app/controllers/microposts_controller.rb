d7be3c177b2b075eb9e9680df2ea188512afa5e1
Nathan Panchal <nathanpanchal@gmail.com>
2016-02-13 12:03:12 -0800

    add destroy functionality to microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     # current_user defined in session helper inherited from Application controller
@@ -14,13 +15,21 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
-
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    # redirects to the previous url
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 
 end
