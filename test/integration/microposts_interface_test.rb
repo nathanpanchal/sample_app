b533f4a58990477b88e6baa8b8b1480f24e06a35
Nathan Panchal <nathanpanchal@gmail.com>
2016-02-23 19:35:46 -0800

    add tests for the micropost interface

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -7,6 +7,27 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTes
   end
 
   test 'micropost interface' do
-
+    log_in_as(@user)
+    get root_path
+    # Attempt to post a micropost without content
+    assert_no_difference 'micropost.count' do
+      post microposts_path, micropost: {content: ''}
+    end
+    assert_select 'div#error_explanation'
+    content = 'This micropost really ties the room together'
+    assert_difference 'Micropost.count', 1 do
+      post microposts_path, micropost: {content: content}
+    end
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_match content, response.body
+    # Delete a post.
+    assert_select 'a', text: 'delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference 'Micropost.count', -1 do
+      delete micropost_path(first_micropost)
+    end
+    get user_path(users(:archer))
+    assert_select 'a', text: 'delete', count: 0
   end
 end
